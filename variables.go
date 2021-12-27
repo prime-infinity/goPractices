aa2db417f98e6244932e9e8904e1d8bb19f84dc6
prime-infinity <soe.a.prime@gmail.com>
2021-12-27 23:13:16 +0100

    done variables

--- variables.go
+++ variables.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+//we can also do the below to group variables
+var (
+	actorName    string = "Elisabeth Sladed"
+	companion    string = "Sarah jane smith"
+	doctorNumber int    = 3
+	season       int    = 11
+)
+
+var I int = 44 //this will be visible to outside pageages cus of the uppercase
+
 func main() {
 
 	//string in go are double quoted
@@ -35,4 +45,22 @@ func main() {
 	scoreThree := 434.12
 	fmt.Println(scoreOne, scoreTwo, scoreThree)
 
+	fmt.Println(actorName)
+
+	//we can also do type conversion from one type to another,
+	//observe
+	var z int = 34
+	var j float32
+	j = float32(z) //z has been converted to float32 and assigned to j
+	fmt.Println(j)
+
 }
+
+/*
+variables cannot be declared twice in the same scope,
+the vraiable with the innermost scope will take presedence
+
+
+also remember that using uppercase var names at package level will make
+the variables visible to outside packages
+*/
