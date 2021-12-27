488f83fd63c69a06f247ef20307029921ead1fb8
prime-infinity <soe.a.prime@gmail.com>
2021-12-28 00:24:30 +0100

    constants

---
+++ constants.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const myConst int = 34
+
+	fmt.Println(myConst)
+}
+
+/*
+ remember, constants have to be available at compile time.
+ constants can be declared without being used immediatly
+
+
+ constants can be any of the primitives types
+
+ constants can also be shadowed like normal variables, inner declareations wind
+*/
