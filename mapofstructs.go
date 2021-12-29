4ca6d807cd4891be47798bc59f94852b5d3d22b3
prime-infinity <soe.a.prime@gmail.com>
2021-12-29 15:53:38 +0100

    mapofstructs

---
+++ mapofstructs.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+type employee struct {
+	salary  int
+	country string
+}
+
+func main() {
+	emp1 := employee{
+		salary:  12000,
+		country: "USA",
+	}
+	emp2 := employee{
+		salary:  14000,
+		country: "Canada",
+	}
+	emp3 := employee{
+		salary:  13000,
+		country: "India",
+	}
+	employeeInfo := map[string]employee{
+		"Steve": emp1,
+		"Jamie": emp2,
+		"Mike":  emp3,
+	}
+
+	for name, info := range employeeInfo {
+		fmt.Printf("Employee: %s Salary:$%d  Country: %s\n", name, info.salary, info.country)
+	}
+
+}
+
+/*
+	this is simbly awesome
+*/
